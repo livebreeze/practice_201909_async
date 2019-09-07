5d851d665f031535f27a5b07cc41a1a44178518f
livebreeze <livebreeze@gmail.com>
2019-09-07 15:38:14 +0800

    my practice

---
+++ S001.MyPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace S001.MyPractice
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine($"step1 {Thread.CurrentThread.ManagedThreadId}");
+            await M1Async(); 
+            Console.WriteLine($"step2 {Thread.CurrentThread.ManagedThreadId}");
+
+            Console.WriteLine("FINISH");
+            Console.ReadKey();
+        }
+
+    
+
+        private static async Task M1Async()
+        {
+            Console.WriteLine($"step3 {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(1000);
+            Console.WriteLine($"step4 {Thread.CurrentThread.ManagedThreadId}");
+        }
+    }
+}
