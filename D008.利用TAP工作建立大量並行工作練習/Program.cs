209f30320721db8b64021dc986f223c375539c82
livebreeze <livebreeze@gmail.com>
2019-09-07 14:48:06 +0800

    refactor: rename and update console.log

--- D008.利用TAP工作建立大量並行工作練習/Program.cs
+++ D008.利用TAP工作建立大量並行工作練習/Program.cs
@@ -16,45 +16,43 @@ namespace D008.利用TAP工作建立大量並行工作練習
             string path = "/api/RemoteSource/Source3";
             string url = $"{host}{path}";
 
-            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();//引用stopwatch物件
+            var sw = new System.Diagnostics.Stopwatch();//引用stopwatch物件
             sw.Reset();//碼表歸零
             sw.Start();//碼表開始計時
-            Console.WriteLine(DateTime.Now);
+            Console.WriteLine($"START!: {DateTime.Now}");
 
-            List<Task> tasks = new List<Task>();
+            var taskList = new List<Task>();
             for (int i = 0; i < 10; i++)
             {
                 var j = string.Format("{0:D2}", i);
-                string task1 = null;
-                string task2 = null;
-
-                var t1 = Task.Run(async () =>
+                var task1 = Task.Run(async () =>
                 {
                     HttpClient client = new HttpClient();
                     Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-1 測試開始時間 {DateTime.Now}");
-                    task1 = await client.GetStringAsync(url);
-                    Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-1 測試結果內容 {task1}");
+                    var result = await client.GetStringAsync(url);
+                    Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-1 測試結果內容 {result}");
                     Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-1 測試結束時間 {DateTime.Now}");
                    
                 });
 
-                var t2 = Task.Run(async () =>
+                var task2 = Task.Run(async () =>
                 {
                     HttpClient client = new HttpClient();
                    
                     Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-2 測試開始時間 {DateTime.Now}");
-                    task2 = await client.GetStringAsync(url);
-                    Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-2 測試結果內容 {task2}");
+                    var result = await client.GetStringAsync(url);
+                    Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-2 測試結果內容 {result}");
                     Console.WriteLine($" {Thread.CurrentThread.ManagedThreadId} :第 {j}-2 測試結束時間 {DateTime.Now}");
                 });
             
-                tasks.Add(t1);
-                tasks.Add(t2);
+                taskList.Add(task1);
+                taskList.Add(task2);
             }
             
-            Task.WaitAll(tasks.ToArray());
+            Task.WaitAll(taskList.ToArray());
             sw.Stop();//碼錶停止
-            Console.WriteLine(sw.Elapsed.TotalMilliseconds.ToString());
+
+            Console.WriteLine($"請求結束，共花 {sw.Elapsed.TotalMilliseconds.ToString()} ms");
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
